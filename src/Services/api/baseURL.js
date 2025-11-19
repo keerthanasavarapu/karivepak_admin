52a29b1f074fb09aa9c5da960e6df379a4d188be
shanthi <santhipriyalattupalli@gmail.com>
2025-11-19 10:02:59 +0530

    url change

--- src/Services/api/baseURL.js
+++ src/Services/api/baseURL.js
@@ -1,7 +1,7 @@
 
 // LIVE URL
 // export const baseURL = 'https://devapigobooze.codefactstech.com/admin';
-// export const baseURL = 'https://api.bodegaa.in'
+export const baseURL = 'https://api.bodegaa.in'
 // export const baseURL ='http://43.204.66.246:5001'
 
 export const productBaseURL = "https://devapigobooze.codefactstech.com/product/v1";
@@ -10,7 +10,7 @@ export const orderURL = 'https://devapigobooze.codefactstech.com/order/api/order
 export const GOOGLE_MAP_API_KEY = "AIzaSyCtTH8DV1-h4tYTSb-geYjdn71a0Up_63k"
 
 // LOCAL URL 
- export const baseURL = 'http://localhost:5001';
+//  export const baseURL = 'http://localhost:5001';
 // export const productBaseURL = "http://localhost:3500/v1";
 //  export const orderURL = 'http://localhost:5002/api/orders';
 // export const variantsBaseURL = "http://localhost:3500/v1/variants";
